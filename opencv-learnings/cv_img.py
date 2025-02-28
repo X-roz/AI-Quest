a0e81fedbefa98748e111c28023bb05e2c7a36e6
Vignesh S <88917157+X-roz@users.noreply.github.com>
2025-02-28 13:07:00 +0530

    Add files via upload

---
+++ opencv-learnings/cv_img.py
@@ -0,0 +1,11 @@
+import cv2
+
+img = cv2.imread("C:/Users/HP/vk-works/AI/Basics-learnings/dataset/img/vicky.jpg")
+while True:
+    cv2.imshow('vicky', img)
+
+    # If we have waited at least 1 ms and the esc button is pressed, then exit
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
