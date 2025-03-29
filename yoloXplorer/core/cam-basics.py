aabed13f2eb04b1755196f082a9909fc816eb6fc
Vignesh S <vigneshkavitha2001@gmail.com>
2025-03-29 21:51:24 +0530

    YOLO - Stream and capturing objects

--- yoloXplorer/core/cam-basics.py
+++ yoloXplorer/core/cam-basics.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
-# import time
+import cvzone
 import cv2
 
 model = YOLO('../weights/yolov8n.pt')
@@ -11,8 +11,18 @@ cam.set(4, 480)
 
 while cam.isOpened():
     ret, frame = cam.read()
-    result = model(frame,show = True)
-    # cv2.imshow("Welcome", result)
+    result = model(frame,stream = True)
+    for r in result:
+        boxes = r.boxes
+        for box in boxes:
+            x1,y1,x2,y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            # cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 5)
+
+            w, h = int(x2-x1), int(y2-y1)
+            cvzone.cornerRect(frame, (x1,y1,w,h))
+
+    cv2.imshow("YOLO SAMPLE", frame)
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
